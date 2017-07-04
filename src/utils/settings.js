a55744ac8aaa776b3e688ecd34739fa3ab8705cb
Sean Yang <xiaofany@hotmail.com>
2017-07-04 15:49:19 -0400

    the settings file

---
+++ src/utils/settings.js
@@ -0,0 +1,11 @@
+'use strict';
+
+let CLOUD_VMS_HOST_NAME = 'https://cloudvms.solinkcloud.com';
+
+if (process.env.REACT_APP_NODE_ENV === 'integration') {
+  CLOUD_VMS_HOST_NAME = 'https://int-cloudvms.solinkcloud.com';
+}
+
+export default Object.assign({}, {
+  cloud_vms_host: CLOUD_VMS_HOST_NAME,
+});
